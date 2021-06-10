c0a353eea27d0b30c22dba440fd6a4578d3bdc66
vinhnx <nxvinh222@gmail.com>
2021-06-10 16:31:50 +0700

    add code view beautify

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,23 +1,44 @@
 import ElementSelector from './ElementSelector'
-import React, { useState, useCallback } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import { Helmet } from "react-helmet";
+import { html, beautify } from 'js-beautify'
+// import { tidy_html5 } from './tidy'
 
 function Home() {
   const [clickedElement, setClickedElement] = useState("Select an element!!!")
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "http://lovasoa.github.io/tidy-html5/tidy.js";
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+  // const 
   let currentElement;
   const handleClickElement = useCallback(val => {
     currentElement = val
-    // console.log("current");
-    // console.log(currentElement)
-    // console.log("outer");
-    // console.log(currentElement.innerHTML)
-    setClickedElement(val.toString());
+    setClickedElement(val.outerHTML);
   }, [setClickedElement]);
+
+  function beautifycode(htmlstr) {
+    htmlstr = htmlstr.split(/\>[ ]?\</).join(">\n<");
+    htmlstr = htmlstr.split(/([*]?\{|\}[*]?\{|\}[*]?)/).join("\n");
+    htmlstr = htmlstr.split(/[*]?\;/).join("\;\n    ");
+    return htmlstr;
+  }
 
   return (
     <div className="row">
+      <Helmet>
+        <script src="http://lovasoa.github.io/tidy-html5/tidy.js" type="text/javascript" />
+      </Helmet>
       <ElementSelector setClickedElement={handleClickElement} />
-      <div className="col-3">
-        {clickedElement}
+      <div className="">
+        <pre>
+          <code>{html(clickedElement, { indent_size: 4, space_in_empty_paren: true })}</code>
+        </pre>
       </div>
     </div>
   );
